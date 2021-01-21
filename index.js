e5b8e666f5d88a28662b11456227ff424f19c470
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-20 17:22:04 -0800

    commit tree a807f29ab72e390e648171b1e8825647c268646c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function createEmployeeRecord(arr) {
+  let employee = {};
+  const [firstName, familyName, title, payPerHour] = arr;
+  employee.firstName = firstName;
+  employee.familyName = familyName;
+  employee.title = title;
+  employee.payPerHour = payPerHour;
+  employee.timeInEvents = [];
+  employee.timeOutEvents = [];
+  return employee;
+}
+function createEmployeeRecords(aOA) {
+  return aOA.map((employee) => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(record, data) {
+  let event = {};
+  event.type = "TimeIn";
+  event.hour = data.split(" ")[1];
+  event.day = data.split("-")[2];
+  record.timeInEvents.push(event);
+  return record;
+}
